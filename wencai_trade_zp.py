82ddc16939be3311fe02c6af4dced0b83979a661
jcl <1427682825@qq.com>
2024-08-22 08:58:55 +0800

    初始化

--- wencai_trade_zp.py
+++ wencai_trade_zp.py
@@ -14,7 +14,7 @@ tdx_client = Quotes.factory(market='std')
 
 
 def get_codes():
-    df = pywencai.get(query='当前涨跌幅大于等于0小于2，流值小于50亿，股价>2且<18,沪深主板，非st，昨日非涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    df = pywencai.get(query='当前涨跌幅>=0且<=2，流值小于50亿，股价>2且<18,沪深主板，创业板，非st，昨日非涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
@@ -31,7 +31,7 @@ def get_data(stock_list):
         df = tdx_client.quotes(symbol=stock_list[i:i + batch_size])
         my_df = pd.concat([my_df, df], ignore_index=True)
     # 过滤条件：reversed_bytes9
-    my_df = my_df[(my_df['reversed_bytes9'] >= 2.5) & (my_df['reversed_bytes9'] <= 3.5)]
+    my_df = my_df[(my_df['reversed_bytes9'] >= 2.5) & (my_df['reversed_bytes9'] <= 4)]
     # 按照Score列进行降序排序，并获取Top 3行
     data = my_df.nlargest(1, 'reversed_bytes9')
     return data
@@ -59,7 +59,7 @@ def job():
         code = data['code']
         price = data['price']
         name = ''
-        enable_balance = 46000
+        enable_balance = 43000
         buy_info(code, float(price), enable_balance, name)
         time.sleep(0.5)
 
