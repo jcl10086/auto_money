16f0545a23c231a0667fe1da3cecee2e5b82c2f4
jcl <1427682825@qq.com>
2024-09-22 12:50:10 +0800

    初始化

--- wencai_trade_zp.py
+++ wencai_trade_zp.py
@@ -1,5 +1,6 @@
 import math
 import time
+from datetime import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from mootdx.quotes import Quotes
@@ -13,8 +14,20 @@ user.prepare('account.json')
 tdx_client = Quotes.factory(market='std')
 
 
-def get_codes():
-    df = pywencai.get(query='开盘涨跌幅>1且<8，沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+def get_codes1():
+    df = pywencai.get(query='开盘涨跌幅>-1且<=0，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    codes = df['code'].values.tolist()
+    return codes
+
+
+def get_codes2():
+    df = pywencai.get(query='开盘涨跌幅>0且<1，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    codes = df['code'].values.tolist()
+    return codes
+
+
+def get_codes3():
+    df = pywencai.get(query='开盘涨跌幅>=1且<2，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
@@ -30,11 +43,9 @@ def get_data(stock_list):
     for i in range(0, len(stock_list), batch_size):
         df = tdx_client.quotes(symbol=stock_list[i:i + batch_size])
         my_df = pd.concat([my_df, df], ignore_index=True)
-    my_df['zf'] = (my_df['price'] - my_df['last_close']) / my_df['last_close'] * 100
     # 过滤条件：reversed_bytes9
-    my_df = my_df[(my_df['zf'] >= 8)]
-    # 按照Score列进行降序排序，并获取Top 3行
-    data = my_df.nlargest(1, 'zf')
+    my_df = my_df[(my_df['reversed_bytes9'] >= 2.5) & (my_df['reversed_bytes9'] <= 4)]
+    data = my_df.nlargest(1, 'reversed_bytes9')
     return data
 
 
@@ -50,20 +61,51 @@ def buy_info(code, price, enable_balance, name):
     return gd_num
 
 
-def job():
-    codes = get_codes()
+def buy(data):
+    code = data['code']
+    price = data['price']
+    name = ''
+    enable_balance = 51500
+    buy_info(code, float(price), enable_balance, name)
+
+
+# 通用的执行函数
+def execute_job(get_codes_func, job_name):
+    codes = get_codes_func()
+    print(f'=====共有{len(codes)}只股票，正在执行{job_name}=====')
+
     while True:
-        data = get_data(codes)
-        if len(data) == 0:
-            print('====执行====')
-            continue
-        code = data['code']
-        price = data['price']
-        name = ''
-        enable_balance = 45000
-        buy_info(code, float(price), enable_balance, name)
-        time.sleep(0.5)
+        # 获取当前时间
+        current_time = datetime.now()
+
+        # 检查时间是否超过 9点30分30秒
+        if current_time.hour > 9 or (current_time.hour == 9 and current_time.minute > 30) or (
+                current_time.hour == 9 and current_time.minute == 30 and current_time.second >= 30):
+
+            # 获取数据
+            data = get_data(codes)
+
+            # 如果数据为空，打印信息并继续
+            if len(data) == 0:
+                print(f'===={job_name}执行====')
+                continue
+
+            # 执行买入操作
+            buy(data)
+
+
+# 定义具体的 job 调用函数
+def job1():
+    execute_job(get_codes1, "Job 1")
+
+
+def job2():
+    execute_job(get_codes2, "Job 2")
+
+
+def job3():
+    execute_job(get_codes3, "Job 3")
 
 
 if __name__ == '__main__':
-    job()
+    job1()
