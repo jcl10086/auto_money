8470a4e5e8a5ba9277c39db6c76040fa740fc933
jcl <1427682825@qq.com>
2024-09-24 13:46:31 +0800

    初始化

--- wencai_trade_zp.py
+++ wencai_trade_zp.py
@@ -17,31 +17,31 @@ tdx_client = Quotes.factory(market='std')
 
 
 def get_codes1():
-    df = pywencai.get(query='开盘涨跌幅>-1且<0，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    df = pywencai.get(query='开盘涨跌幅>-0.5且<0，流值小于50亿，股价>1且<12,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
 
 def get_codes2():
-    df = pywencai.get(query='开盘涨跌幅>0且<=0.3，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    df = pywencai.get(query='开盘涨跌幅>0且<=0.3，流值小于50亿，股价>1且<12,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
 
 def get_codes3():
-    df = pywencai.get(query='开盘涨跌幅>=1且<2，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    df = pywencai.get(query='开盘涨跌幅>=1且<2，流值小于50亿，股价>1且<12,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
 
 def get_codes4():
-    df = pywencai.get(query='开盘涨跌幅=0，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    df = pywencai.get(query='开盘涨跌幅=0，流值小于50亿，股价>1且<12,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
 
 def get_codes5():
-    df = pywencai.get(query='开盘涨跌幅>0.3且<1，流值小于50亿，股价>2且<18,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
+    df = pywencai.get(query='开盘涨跌幅>0.3且<1，流值小于50亿，股价>1且<12,沪深主板，非st，昨日未涨停且前日未涨停', loop=True, sort_order='desc', sort_key='最新涨跌幅')
     codes = df['code'].values.tolist()
     return codes
 
@@ -58,11 +58,11 @@ def get_data(stock_list):
         df = tdx_client.quotes(symbol=stock_list[i:i + batch_size])
         my_df = pd.concat([my_df, df], ignore_index=True)
 
-    my_df['max_zf'] = (my_df['high'] - my_df['last_close']) / my_df['last_close'] * 100
-    my_df['min_zf'] = (my_df['low'] - my_df['last_close']) / my_df['last_close'] * 100
+    # my_df['max_zf'] = (my_df['high'] - my_df['last_close']) / my_df['last_close'] * 100
+    # my_df['min_zf'] = (my_df['low'] - my_df['last_close']) / my_df['last_close'] * 100
     # 过滤条件：reversed_bytes9
-    my_df = my_df[(my_df['reversed_bytes9'] >= 3) & (my_df['reversed_bytes9'] <= 5)]
-    my_df = my_df[(my_df['min_zf'] >= -2) & (my_df['max_zf'] <= 7)]
+    my_df = my_df[(my_df['reversed_bytes9'] >= 3) & (my_df['reversed_bytes9'] <= 5) & (my_df['vol'] > 2000)]
+    # my_df = my_df[(my_df['min_zf'] >= -2) & (my_df['max_zf'] <= 7)]
     data = my_df.nlargest(1, 'reversed_bytes9')
     return data
 
@@ -83,7 +83,7 @@ def buy(data):
     code = data['code']
     price = data['price']
     name = ''
-    enable_balance = 51500
+    enable_balance = 50700
     buy_info(code, float(price), enable_balance, name)
 
 
@@ -93,23 +93,22 @@ def execute_job(get_codes_func, job_name):
     print(f'=====共有{len(codes)}只股票，正在执行{job_name}=====')
 
     while True:
-        # 获取当前时间
-        current_time = datetime.now()
+        # # 获取当前时间
+        # current_time = datetime.now()
+        #
+        # # 检查时间是否超过 9点30分30秒
+        # if current_time.hour > 9 or (current_time.hour == 9 and current_time.minute > 31) or (
+        #         current_time.hour == 9 and current_time.minute == 31 and current_time.second >= 10):
+        # 获取数据
+        data = get_data(codes)
+        # 如果数据为空，打印信息并继续
+        if len(data) == 0:
+            print(f'===={job_name}执行====')
+            continue
+        # 执行买入操作
+        buy(data)
 
-        # 检查时间是否超过 9点30分30秒
-        if current_time.hour > 9 or (current_time.hour == 9 and current_time.minute > 30) or (
-                current_time.hour == 9 and current_time.minute == 30 and current_time.second >= 30):
 
-            # 获取数据
-            data = get_data(codes)
-
-            # 如果数据为空，打印信息并继续
-            if len(data) == 0:
-                print(f'===={job_name}执行====')
-                continue
-
-            # 执行买入操作
-            buy(data)
 
 
 # 定义具体的 job 调用函数
