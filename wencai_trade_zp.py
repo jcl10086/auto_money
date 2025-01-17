35e1bd6eabf3a0ceb833f17de5d582d5591298b1
jcl <1427682825@qq.com>
2025-01-17 09:38:52 +0800

    初始化

--- wencai_trade_zp.py
+++ wencai_trade_zp.py
@@ -61,7 +61,7 @@ def get_data(stock_list):
     # my_df['max_zf'] = (my_df['high'] - my_df['last_close']) / my_df['last_close'] * 100
     # my_df['min_zf'] = (my_df['low'] - my_df['last_close']) / my_df['last_close'] * 100
     # 过滤条件：reversed_bytes9
-    my_df = my_df[(my_df['reversed_bytes9'] >= 2) & (my_df['vol'] > 1000)]
+    my_df = my_df[(my_df['reversed_bytes9'] >= 3) & (my_df['vol'] > 1000)]
     # my_df = my_df[(my_df['min_zf'] >= -2) & (my_df['max_zf'] <= 7)]
     data = my_df.nlargest(1, 'reversed_bytes9')
     return data
