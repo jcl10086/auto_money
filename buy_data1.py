e5214058e2a642698a6eef071ec944947392c323
jcl <1427682825@qq.com>
2025-02-17 19:51:23 +0800

    涨停买入

--- buy_data1.py
+++ buy_data1.py
@@ -32,7 +32,7 @@ def get_data(stock_list):
     # 涨幅
     my_df['zf'] = (my_df['price'] - my_df['last_close']) / my_df['last_close'] * 100
     my_df['zt_price'] = round(my_df['last_close'] * 1.1, 2)
-    my_df = my_df[(my_df['reversed_bytes9'] >= 1) & (my_df['zf'] >= 9)]
+    my_df = my_df[(my_df['reversed_bytes9'] >= 2) & (my_df['zf'] >= 9)]
     data = my_df.nlargest(1, 'reversed_bytes9')
     return data
 
