fc68e9c87044525034ad894c92b640b52dba4732
jcl <1427682825@qq.com>
2025-03-04 09:21:59 +0800

    涨停买入

--- db_robot.py
+++ db_robot.py
@@ -44,7 +44,7 @@ def get_data(stock_list):
     # 涨幅
     my_df['zf'] = (my_df['price'] - my_df['last_close']) / my_df['last_close'] * 100
     my_df['zt_price'] = round(my_df['last_close'] * 1.1, 2)
-    my_df = my_df[(my_df['reversed_bytes9'] >= 1) & (my_df['price'] == my_df['zt_price']) & (my_df['ask_vol1'] < 3000)]
+    my_df = my_df[(my_df['reversed_bytes9'] >= 1) & (my_df['price'] == my_df['zt_price']) & (my_df['ask_vol1'] < 5000)]
     data = my_df.nlargest(1, 'reversed_bytes9')
     return data
 
@@ -54,7 +54,7 @@ def buy(data):
     code = data['code'].values[0]
     # 涨停买入
     price = data['zt_price']
-    enable_balance = 196000
+    enable_balance = 51000
     buy_info(code, float(price), enable_balance)
 
 
