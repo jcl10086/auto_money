59b4cc651492894c40ba27666946cf01644e345b
jcl <1427682825@qq.com>
2025-07-25 16:39:28 +0800

    level2 交易

--- sell_zt.py
+++ sell_zt.py
@@ -2,6 +2,7 @@ import time
 from datetime import datetime
 
 import pandas as pd
+import requests
 from mootdx.quotes import Quotes
 
 import easytrader
@@ -14,17 +15,16 @@ user = easytrader.use('eastmoney')
 user.prepare('account.json')
 
 
-def get_info(stock_list):
+def get_info(code):
+    url = f"http://121.43.57.182:21936/sql?token=fb6d25972a7bb566a74cf69c853e5d74&mode=level_queue&code={code}"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    rs_json = response.json()
+    fd = rs_json['data']['list'][0]['volume_count']
     current_time = datetime.now()
-    # 格式化时间
-    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
-    my_df = None
-    batch_size = 50
-    for i in range(0, len(stock_list), batch_size):
-        df = tdx_client.quotes(symbol=stock_list[i:i + batch_size])
-        my_df = pd.concat([my_df, df], ignore_index=True)
-    fd = my_df['bid_vol1'].values[0]
-    print(f'时间：{formatted_time} ================================ 封单：{fd}')
+    ts = current_time.strftime('%H:%M:%S.%f')[:-3]
+    print(f'时间：{ts} ================================ 封单：{fd}')
     return fd
 
 
@@ -33,7 +33,7 @@ def sell(code, gd_price, enable_amount):
 
 
 if __name__ == '__main__':
-    stock_list = ['002266']
+    code = ['002266']
     gd_price = 4.42
     enable_amount = 7300
     # 封单阀值
@@ -42,13 +42,13 @@ if __name__ == '__main__':
         # 获取当前时间
         now = datetime.now().time()
         # 设定一个指定的时间点，比如 14:30
-        target_time = datetime.strptime("17:55", "%H:%M").time()
-        # 判断当前时间是否大于指定时间
-        if now >= target_time:
-            print(f'时间：{now} ================================ 超出时间范围')
-            break
-        fd = get_info(stock_list)
+        # target_time = datetime.strptime("14:55", "%H:%M").time()
+        # # 判断当前时间是否大于指定时间
+        # if now >= target_time:
+        #     print(f'时间：{now} ================================ 超出时间范围')
+        #     break
+        fd = get_info(code[0])
         if fd < fd_num:
-            sell(stock_list[0], gd_price, enable_amount)
+            sell(code[0], gd_price, enable_amount)
             break
-        time.sleep(0.5)
+        # time.sleep(0.5)
