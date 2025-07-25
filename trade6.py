98541f4254fd4f15c51f2d3e8bcabcbcd4efd16f
jcl <1427682825@qq.com>
2025-07-25 14:26:46 +0800

    level2 交易

--- trade6.py
+++ trade6.py
@@ -1,7 +1,7 @@
 #!python3
 # -*- coding:utf-8 -*-
 # 依赖安装: pip install websocket-client
-# level2 打板
+# level2 打板  逐笔成交
 import math
 import sys
 import time
@@ -13,20 +13,27 @@ import pandas as pd
 import websocket
 import zlib
 import pywencai
+from mootdx.quotes import Quotes
+
 import easytrader
 
 # 初始化账号
 user = easytrader.use('eastmoney')
 user.prepare('account.json')
+tdx_client = Quotes.factory(market='std')
 cookie = 'other_uid=Ths_iwencai_Xuangu_hg54pqsca5cpwxmxubzrnmu9gxl5bzmx; ta_random_userid=h7x5hms3yy; u_ukey=A10702B8689642C6BE607730E11E6E4A; u_uver=1.0.0; u_dpass=umhwFo6FJF%2FqZxPCdx%2BQaGVVO4BxcIgFcA4HC96P2H%2F18m26lRsmsSJuBvQEJCdBHi80LrSsTFH9a%2B6rtRvqGg%3D%3D; u_did=7367FAF6251D45A88CB536A1C4018DCA; u_ttype=WEB; user_status=0; user=MDptb181NzA4OTQ0MTQ6Ok5vbmU6NTAwOjU4MDg5NDQxNDo1LDEsNDA7NiwxLDQwOzcsMTExMTExMTExMTEwLDQwOzgsMTExMTAxMTEwMDAwMTExMTEwMDEwMDEwMDEwMDAwMDAsNDA7MzMsMDAwMTAwMDAwMDAwLDEyODszNiwxMDAxMTExMTAwMDAxMTAwMTAxMTExMTEsMTI4OzQ2LDAwMDAxMTExMTAwMDAwMTExMTExMTExMSwxMjg7NTEsMTEwMDAwMDAwMDAwMDAwMCwxMjg7NTgsMDAwMDAwMDAwMDAwMDAwMDEsMTI4Ozc4LDEsMTI4Ozg3LDAwMDAwMDAwMDAwMDAwMDAwMDAxMDAwMCwxMjg7MTAzLDAwMDAwMDEwMDAwMDAwMDAsMTI4OzExNSwwMDAwMDAwMDAwMDAwMDEwMDAwMDAwMDAwMDAwMDAwMCwxMjg7MTE5LDAwMDAwMDAwMDAwMDAwMDAwMDEwMTAwMDAwMDAwMCwxMjg7MTI1LDExLDEyODs0NCwxMSw0MDsxLDEwMSw0MDsyLDEsNDA7MywxLDQwOzEwMiwxLDQwOjI0Ojo6NTcwODk0NDE0OjE3NDY1OTMxMjM6OjoxNjE1MDExMjQwOjI2Nzg0MDA6MDoxYzU4MWM0Njk4Njg0MmQwNTAwYTE2MzEyYzZiNDUyMjQ6ZGVmYXVsdF80OjE%3D; userid=570894414; u_name=mo_570894414; escapename=mo_570894414; ticket=a4f1039da2680a51c6355cc9d440a424; utk=43f2e2dbf1646ceff09a37a3fb4556c9; v=A69j3La3GzxpMB8AVAkjzpsoPsi7VAMvnagHacE9S54lEMG2ySSTxq14l6DS'
 codes = []
+my_df = None
 
 def trade_data(results):
     flag = False
     df = pd.DataFrame(results)
+
+    merged = pd.merge(df, my_df, on='code', how='inner')
+    df = merged[['code', 'current_price', 'zt_price']]
     print(df)
     # df = df[(df['buy1_price'] == df['zt_price']) & (df['buy1_quantity'] > 5000000)]
-    df = df[df['buy1_price'].astype(float) >= (df['zt_price'].astype(float) * 0.995).round(2)]
+    df = df[df['current_price'].astype(float) >= (df['zt_price'].astype(float) * 0.995).round(2)]
     data = df.nsmallest(1, 'zt_price')
     # 如果数据为空，打印信息并继续
     if len(data) == 0:
@@ -37,9 +44,10 @@ def trade_data(results):
         flag = True
     return flag
 
+
 def get_codes():
     global codes
-    df = pywencai.get(query='开盘涨跌幅>7，开盘未涨停，昨日非连板，非京市，非st，非科创板，非创业板', loop=True, sort_order='desc', sort_key='最新涨跌幅', pro=True, cookie=cookie)
+    df = pywencai.get(query='曾涨停，昨日未涨停，沪深主板非st', loop=True, sort_order='desc', sort_key='最新涨跌幅', pro=True, cookie=cookie)
     codes = df['code'].values.tolist()
 
     # 移除数组
@@ -51,10 +59,24 @@ def get_codes():
     return codes
 
 
+def get_data(stock_list):
+    # stock_list = stock_list[0:1]
+    global my_df
+    batch_size = 50
+    for i in range(0, len(stock_list), batch_size):
+        df = tdx_client.quotes(symbol=stock_list[i:i + batch_size])
+        my_df = pd.concat([my_df, df], ignore_index=True)
+
+    my_df['zt_price'] = round(my_df['last_close'] * 1.1, 2)
+    # my_df = my_df[(my_df['min_zf'] >= -2) & (my_df['max_zf'] <= 7)]
+    # data = my_df.nsmallest(1, 'price')
+    return my_df
+
+
 # 发送订阅
 def on_open(ws):
     # 用列表推导式和 join 拼接
-    all_codes = "all=" + ",".join([f"lv10_{code}" for code in codes])
+    all_codes = "all=" + ",".join([f"lv2_{code}" for code in codes])
     ws.send(all_codes)
 
 
@@ -67,7 +89,7 @@ def on_message(ws, message, type, flag):
     if type == websocket.ABNF.OPCODE_BINARY:
         rb = zlib.decompress(message, -zlib.MAX_WBITS)
         # print(time.strftime('%H:%M:%S', time.localtime(time.time())), "Binary响应:", rb.decode("utf-8"))
-        results = parse_level10_data(rb.decode("utf-8"))
+        results = parse_level2_data(rb.decode("utf-8"))
         if trade_data(results):
             exit()
 
@@ -80,31 +102,18 @@ def on_close(ws, code, msg):
     print(time.strftime('%H:%M:%S', time.localtime(time.time())), "连接已断开")
 
 
-def parse_level10_data(data):
+def parse_level2_data(data):
     results = []
-    for line in data.strip().split('\n'):
-        if not line.startswith('lv10_'):
-            continue
-        # 分割证券代码和行情数据
-        code_part, values = line.split('=', 1)
-        code = code_part.replace('lv10_', '')
-        # 分割各字段数据
-        fields = values.split(',')
-        # 提取关键数据
-        prev_close = float(fields[3])  # 昨收价
-        # 买一价格（买十档的第一个价格）
-        buy1_price = float(fields[6])
-        # 买一数量（买十档的第一个数量，位于买十档价格后的第10个位置）
-        buy1_quantity = int(fields[16])
-        # 计算涨停价（昨收的1.1倍，四舍五入保留2位小数）
-        zt_price = round(prev_close * 1.1, 2)
-        result = {
-           "code": code,
-            "buy1_price": buy1_price,
-            "buy1_quantity": buy1_quantity,
-            "zt_price": zt_price
-        }
-        results.append(result)
+    code_part, values = data.split('=', 1)
+    code = code_part.replace('lv2_', '')
+    segments  = values.split('|')
+    last_segment = segments[-1]
+    current_price = last_segment.split(',')[2]
+    result ={
+        'code': code,
+        'current_price': current_price
+    }
+    results.append(result)
     return results
 
 
@@ -139,7 +148,7 @@ def buy(data):
     name = ''
     # enable_balance = 190000
     # enable_balance = get_balance()
-    enable_balance = 30000
+    enable_balance = 30500
     rs = buy_info(code, float(price), enable_balance, name, zt_price)
     return rs
 
@@ -164,6 +173,7 @@ if __name__ == '__main__':
         time.sleep(5)
 
     codes = get_codes()
+    my_df = get_data(codes)
     print(f'=====共有{len(codes)}只股票=====')
     ws.run_forever()
     # while True:
