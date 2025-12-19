6845aea97f296329eb638abee0fad966e8144bcf
jcl <1427682825@qq.com>
2025-12-19 09:42:42 +0800

    打板-涨停交易量

--- buy_db_jyl.py
+++ buy_db_jyl.py
@@ -180,7 +180,7 @@ def buy(data):
     name = ''
     # enable_balance = 190000
     # enable_balance = get_balance()
-    enable_balance = 116000
+    enable_balance = 112400
     rs = buy_info(code, float(price), enable_balance, name, zt_price)
     return rs
 
