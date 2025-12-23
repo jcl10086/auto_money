742c9105647c791d51a72b2aee83b13f98966b2f
jcl <1427682825@qq.com>
2025-12-23 13:16:44 +0800

    秒板

--- buy_db_mb.py
+++ buy_db_mb.py
@@ -62,7 +62,7 @@ def trade_data(results):
 
 def get_codes():
     global codes
-    df = pywencai.get(query='沪深主板非st，昨日未涨停，前日未涨停，当前涨幅>2.5且<8，开盘涨跌幅<4', loop=True, sort_order='desc', sort_key='最新涨跌幅', pro=True, cookie=cookie)
+    df = pywencai.get(query='沪深主板非st，昨日未涨停，前日未涨停，当前涨幅>2.5且<8，流值小于120亿，今日最大涨幅<9.6且>7', loop=True, sort_order='desc', sort_key='最新涨跌幅', pro=True, cookie=cookie)
     codes = df['code'].values.tolist()
 
     # 移除数组
@@ -108,7 +108,7 @@ def on_message(ws, message, type, flag):
         print(f"================执行次数：{count}================")
         rb = zlib.decompress(message, -zlib.MAX_WBITS)
         # print(time.strftime('%H:%M:%S', time.localtime(time.time())), "Binary响应:", rb.decode("utf-8"))
-        if count >= 50:
+        if count >= 200:
             ws.close()
         results = parse_level2_data(rb.decode("utf-8"))
         if trade_data(results):
@@ -175,7 +175,7 @@ def buy(data):
     name = ''
     # enable_balance = 190000
     # enable_balance = get_balance()
-    enable_balance = 116000
+    enable_balance = 54000
     rs = buy_info(code, float(price), enable_balance, name, zt_price)
     return rs
 
